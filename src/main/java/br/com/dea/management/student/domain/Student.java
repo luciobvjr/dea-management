b56a81316ef173c7bef0e70452d18aa89c7fb1f4
Lucio Bueno Vieira Junior <luciobvjr@gmail.com>
2023-01-30 11:42:20 -0300

    Created Student domain

---
+++ src/main/java/br/com/dea/management/student/domain/Student.java
@@ -0,0 +1,67 @@
+package br.com.dea.management.student.domain;
+
+import br.com.dea.management.user.domain.User;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column
+    private String university;
+
+    @Column
+    private String graduation;
+
+    @Column
+    private LocalDate finish_date;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUniversity() {
+        return university;
+    }
+
+    public void setUniversity(String university) {
+        this.university = university;
+    }
+
+    public String getGraduation() {
+        return graduation;
+    }
+
+    public void setGraduation(String graduation) {
+        this.graduation = graduation;
+    }
+
+    public LocalDate getFinish_date() {
+        return finish_date;
+    }
+
+    public void setFinish_date(LocalDate finish_date) {
+        this.finish_date = finish_date;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
