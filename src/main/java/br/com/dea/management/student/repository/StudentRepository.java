205a61bf12b64548a9a72969dc8849441c17329f
Lucio Bueno Vieira Junior <luciobvjr@gmail.com>
2023-01-30 11:47:08 -0300

    Created StudentRepository with paginated query

---
+++ src/main/java/br/com/dea/management/student/repository/StudentRepository.java
@@ -0,0 +1,14 @@
+package br.com.dea.management.student.repository;
+
+import br.com.dea.management.student.domain.Student;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    @Query("SELECT s FROM Student s")
+    Page<Student> findAllPaginated(Pageable pageable);
+}
