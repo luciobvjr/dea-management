1254af0f326db340f66b73f1f29de00c839d849e
Lucio Bueno Vieira Junior <luciobvjr@gmail.com>
2023-02-07 14:43:40 -0300

    Add lombok to project / Getters and Setter to User and Student domains

--- src/main/java/br/com/dea/management/student/domain/Student.java
+++ src/main/java/br/com/dea/management/student/domain/Student.java
@@ -2,10 +2,14 @@ package br.com.dea.management.student.domain;
 
 import br.com.dea.management.user.domain.User;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Entity
+@Getter
+@Setter
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,44 +28,4 @@ public class Student {
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "user_id", referencedColumnName = "id")
     private User user;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUniversity() {
-        return university;
-    }
-
-    public void setUniversity(String university) {
-        this.university = university;
-    }
-
-    public String getGraduation() {
-        return graduation;
-    }
-
-    public void setGraduation(String graduation) {
-        this.graduation = graduation;
-    }
-
-    public LocalDate getFinishDate() {
-        return finishDate;
-    }
-
-    public void setFinishDate(LocalDate finish_date) {
-        this.finishDate = finish_date;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
 }
--- src/main/java/br/com/dea/management/user/domain/User.java
+++ src/main/java/br/com/dea/management/user/domain/User.java
@@ -1,8 +1,12 @@
 package br.com.dea.management.user.domain;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 @NamedQuery(name = "linkedinQuery", query = "SELECT u FROM User u WHERE linkedin = :linkedin")
 public class User {
     @Id
@@ -21,44 +25,4 @@ public class User {
 
     @Column
     private String linkedin;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getLinkedin() {
-        return linkedin;
-    }
-
-    public void setLinkedin(String linkedin) {
-        this.linkedin = linkedin;
-    }
 }
