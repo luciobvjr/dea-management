5dca275eb3e4260724e10f89c3392126406dffe4
Lucio Bueno Vieira Junior <luciobvjr@gmail.com>
2023-03-14 20:48:50 -0300

    Employee and Position domain

---
+++ src/main/java/br/com/dea/management/employee/domain/Employee.java
@@ -0,0 +1,32 @@
+package br.com.dea.management.employee.domain;
+
+import br.com.dea.management.employee.EmployeeType;
+import br.com.dea.management.position.domain.Position;
+import br.com.dea.management.user.domain.User;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.validator.constraints.ISBN;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private EmployeeType employeeType;
+
+    @ManyToOne
+    @JoinColumn(name = "position_id")
+    private Position position;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+}
