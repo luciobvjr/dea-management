412eb04262f8efcb50e5f8c3ea3f9300488a5486
Lucio Bueno Vieira Junior <luciobvjr@gmail.com>
2023-04-11 16:19:07 -0300

    Created AcademyClassRepository

---
+++ src/main/java/br/com/dea/management/academyclass/AcademyClassRepository.java
@@ -0,0 +1,14 @@
+package br.com.dea.management.academyclass;
+
+import br.com.dea.management.academyclass.domain.AcademyClass;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface AcademyClassRepository extends JpaRepository<AcademyClass, Long> {
+    @Query("SELECT a FROM AcademyClass a")
+    @EntityGraph(attributePaths = {"instructor", "instructor.user", "instructor.position"})
+    public Page<AcademyClass> findAllPaginated(Pageable pageable);
+}
