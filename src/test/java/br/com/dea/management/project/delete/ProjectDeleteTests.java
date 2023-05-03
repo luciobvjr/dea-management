e0a16135b7f42449eac06ab9291456e542064d56
Lucio Bueno Vieira Junior <luciobvjr@gmail.com>
2023-05-03 10:30:07 -0300

    Delete Project Tests

---
+++ src/test/java/br/com/dea/management/project/delete/ProjectDeleteTests.java
@@ -0,0 +1,83 @@
+package br.com.dea.management.project.delete;
+
+import br.com.dea.management.employee.EmployeeTestUtils;
+import br.com.dea.management.employee.repository.EmployeeRepository;
+import br.com.dea.management.project.ProjectTestUtils;
+import br.com.dea.management.project.domain.Project;
+import br.com.dea.management.project.repository.ProjectRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class ProjectDeleteTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+
+    @Autowired
+    private ProjectTestUtils projectTestUtils;
+
+    @Autowired
+    private EmployeeTestUtils employeeTestUtils;
+
+    public static final MediaType APPLICATION_JSON_UTF8 = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(), StandardCharsets.UTF_8);
+
+    @Test
+    void whenRequestingToRemoveProject_thenRemoveAProjectSuccessfully() throws Exception {
+        this.projectRepository.deleteAll();
+        this.employeeRepository.deleteAll();
+
+        LocalDate startDate = LocalDate.of(2021, 3,1);
+        LocalDate endDate = LocalDate.of(2022, 12, 4);
+        Project project = this.projectTestUtils.createFakeProject(startDate, endDate);
+
+        mockMvc.perform(delete("/project/" + project.getId())
+                        .contentType(APPLICATION_JSON_UTF8))
+                        .andExpect(status().isOk());
+
+        List<Project> projects = this.projectRepository.findAll();
+
+        assertThat(projects.size()).isEqualTo(0);
+    }
+
+    @Test
+    void whenRemovingAEmployeeThatDoesNotExists_thenReturn404() throws Exception {
+        this.projectRepository.deleteAll();
+        this.employeeRepository.deleteAll();
+
+        mockMvc.perform(delete("/project/0")
+                        .contentType(APPLICATION_JSON_UTF8))
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.message").exists())
+                .andExpect(jsonPath("$.details").isArray())
+                .andExpect(jsonPath("$.details", hasSize(1)));
+    }
+}
